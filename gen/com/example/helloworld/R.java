0eb2bd8e7a858cb488971316753af8ad50a0904c
vjuge <vincent.juge@gmail.com>
2014-05-06 13:24:16 +0200

    tuto

--- gen/com/example/helloworld/R.java
+++ gen/com/example/helloworld/R.java
@@ -1080,6 +1080,8 @@ containing a value of this type.
         public static final int ic_launcher=0x7f020057;
     }
     public static final class id {
+        public static final int abcPoids=0x7f05003d;
+        public static final int abcTaille=0x7f05003f;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -1091,27 +1093,33 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003f;
+        public static final int action_settings=0x7f050049;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
-        public static final int button1=0x7f05003e;
+        public static final int butCalcIMC=0x7f050045;
+        public static final int butRAZ=0x7f050046;
         public static final int checkbox=0x7f05002d;
+        public static final int checkmegaFonction=0x7f050044;
         public static final int collapseActionView=0x7f05000d;
         public static final int container=0x7f05003c;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
         public static final int dropdown=0x7f05000f;
+        public static final int editPoids=0x7f05003e;
+        public static final int editTaille=0x7f050040;
         public static final int edit_query=0x7f050030;
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
+        public static final int grosBoutton=0x7f050048;
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
+        public static final int imcResult=0x7f050047;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
@@ -1121,6 +1129,9 @@ containing a value of this type.
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
+        public static final int radioCm=0x7f050043;
+        public static final int radioGroup1=0x7f050041;
+        public static final int radioMetre=0x7f050042;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
         public static final int search_button=0x7f050033;
@@ -1138,7 +1149,6 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
-        public static final int textView1=0x7f05003d;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
@@ -1197,13 +1207,17 @@ containing a value of this type.
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_main=0x7f030018;
         public static final int fragment_main=0x7f030019;
-        public static final int layout2=0x7f03001a;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001b;
+        public static final int grosboutton=0x7f03001a;
+        public static final int layout2=0x7f03001b;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001c;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
     }
     public static final class string {
+        public static final int ImcResult=0x7f0a0019;
+        public static final int RAZ=0x7f0a0017;
+        public static final int abcClicInvite=0x7f0a0010;
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
          */
         public static final int abc_action_bar_home_description=0x7f0a0001;
@@ -1246,7 +1260,14 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
+        public static final int calcIMC=0x7f0a0016;
+        public static final int cm=0x7f0a0014;
+        public static final int grosbutton=0x7f0a0018;
         public static final int hello_world=0x7f0a000e;
+        public static final int megaFonction=0x7f0a0015;
+        public static final int metre=0x7f0a0013;
+        public static final int poids=0x7f0a0011;
+        public static final int taille=0x7f0a0012;
     }
     public static final class style {
         /** 
--- src/com/example/helloworld/MainActivity.java
+++ src/com/example/helloworld/MainActivity.java
@@ -1,24 +1,62 @@
 package com.example.helloworld;
 
-import android.os.Bundle;
-import android.widget.TextView;
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-	public void onCreate(Bundle savedInstanceState) {
-		TextView textH = null;
-		String hello = null;
-		super.onCreate(savedInstanceState);
-	    
-		hello = getResources().getString(R.string.hello_world);
-		hello = hello.replace("world", "les zéros");
-		
-	    textH = new TextView(this);
-	    textH.setText(hello);
-	    setContentView(textH);
+	Button bcalcIMC, grosButton, bRAZ = null;
+	TextView resultIMC = null;
+	EditText edTaille, edPoids = null; 
 
-	    
-	  }
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.fragment_main);
+		bcalcIMC = (Button) findViewById(R.id.butCalcIMC);
+		bRAZ = (Button) findViewById(R.id.butRAZ);
+		resultIMC = (TextView) findViewById(R.id.imcResult);
+		edPoids = (EditText) findViewById(R.id.editPoids);
+		edTaille = (EditText) findViewById(R.id.editTaille);
+//		grosButton = (Button) findViewById(R.id.grosBoutton);
+//
+//		b1.setOnTouchListener(touchListenerButton1);
+//		b1.setOnClickListener(clickListenerButton1);
+//
+//		grosButton.setOnTouchListener(new View.OnTouchListener() {
+//			@Override
+//			public boolean onTouch(View v, MotionEvent event) {
+//				// TODO Auto-generated method stub
+//				// return false;
+//				float posx = event.getX();
+//				float posy = event.getY();
+//				int width = grosButton.getWidth();
+//				int height = grosButton.getHeight();
+//				grosButton.setTextSize(Math.abs(posx - width / 2)
+//						+ Math.abs(posy - height / 2));
+//				return true;
+//				// return onTouch(v, event);
+//			}
+//		});
+
+		
+		bcalcIMC.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				//resultIMC
+				float calcRes, t, p;
+				t = Float.valueOf(edTaille.getText().toString());
+				p = Float.valueOf(edPoids.getText().toString());
+				calcRes = p / (float)Math.pow(t, 2);
+				resultIMC.setText(String.valueOf(calcRes));
+			}
+		});
+	}
 	
-    }
+}
